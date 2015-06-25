f94bbbcda56fe830f940ab4fcc61e7e7eb44894f
Alan <richard.alan.oli@gmail.com>
2015-06-25 18:32:36 -0500

    ADD structure DB

--- database/connection/mysql_connection.js
+++ database/connection/mysql_connection.js
@@ -10,3 +10,15 @@ connection.query("USE db_prueba;")
 
 module.exports = connection;
 
+
+/**
+*  Structure DB
+*
+CREATE DATABASE db_prueba;
+USE db_prueba;
+CREATE TABLE user(
+user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+user_name VARCHAR(30)
+);
+*/
+
